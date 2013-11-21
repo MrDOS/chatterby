e6628e0fef7319abc71c8eb6130581b44bf2ff65
scoleman <scoleman@cb86ddd3-c1bc-4ab9-81f3-b45283a3f62c>
2013-11-21 05:10:42 +0000

    Message colourization and formatting. Fixed visible refreshing on message append.

---
+++ trunk/src/chatterby/ui/Colorizer.java
@@ -0,0 +1,33 @@
+package chatterby.ui;
+
+import java.awt.Color;
+
+/**
+ * Picks a color based on some data.
+ * 
+ * @author scoleman
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Colorizer
+{
+    private static Color[] COLORS = new Color[] {
+            Color.BLUE,
+            Color.CYAN.darker(),
+            Color.GREEN.darker(),
+            Color.MAGENTA,
+            Color.ORANGE,
+            // Color.PINK, /* The pink is hideous. */
+            Color.RED,
+            Color.YELLOW.darker()
+    };
+
+    public static Color colorize(String s)
+    {
+        int codePointSum = 0;
+        for (int i = 0; i < s.length(); i++)
+            codePointSum += s.codePointAt(i);
+
+        return Colorizer.COLORS[codePointSum % Colorizer.COLORS.length];
+    }
+}
